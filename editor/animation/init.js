339e311710690b938c35866fbc5472b0aeb6fea2
Oleksandr Liabakh <oleksandrliabakh@Oleksandrs-MacBook.local>
2016-07-19 16:32:20 +0300

    js version

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,5 +1,81 @@
+
 requirejs(['ext_editor_io', 'jquery_190', 'raphael_210'],
     function (extIO, $, TableComponent) {
+
+        function convertStringsTo2D (strings) {
+            var strings2D = [];
+            for (var i =0; i < strings.length; i++) {
+                strings2D.push(strings[i].split(''));
+            }
+            return strings2D;
+        }
+
+        function createTableFrom2D (array2D ) {
+            var $table = $("<table></table>");
+            for (var i=0; i < array2D.length; i++) {
+                var $tr = $("<tr></tr>");
+                for (var j=0; j < array2D[i].length; j++) {
+                    $tr.append($("<td>").html(array2D[i][j]))
+                }
+                $table.append($tr);
+            }
+            return $table;
+        }
+
+        function rotateSquareMatrixCW (matrix) {
+            var rotatedMatrix = [];
+            var N = matrix.length;
+            for (var i=0; i < N; i++) {
+                var tempArray = [];
+                for (var j=0; j < N; j++) {
+                    tempArray.push(matrix[N-j-1][i]);
+                }
+                rotatedMatrix.push(tempArray);
+            }
+            return rotatedMatrix;
+        }
+
+        function mergeGrillePassword (grille, cpassword) {
+            var resultMatrix = [];
+            var resultStr = '';
+            for (var i=0; i < grille.length; i++) {
+                var tempArray = [];
+                for (var j=0; j < grille[i].length; j++) {
+                    if (grille[i][j] == 'X') {
+                        tempArray.push(cpassword[i][j]);
+                        resultStr += cpassword[i][j];
+                    }
+                    else {
+                        tempArray.push('.');
+                    }
+                }
+                resultMatrix.push(tempArray);
+            }
+            return [resultMatrix, resultStr];
+        }
+
+        function collectDataFromTable ($table, size) {
+            var array2D = [];
+            var tds = $table.find("td");
+            var vsize = Math.floor(tds.length / size);
+            for (var i=0; i < vsize; i++) {
+                var tempArray = [];
+                for (var j=0; j <size; j++) {
+                    var k = i * vsize + j;
+                    var $inElement = $(tds[k]).find("*");
+                    tempArray.push($inElement.html() || $inElement.val());
+                }
+                array2D.push(tempArray);
+            }
+            return array2D;
+        }
+
+        function fillBlue() {
+            if ($(this).html() == '.') {
+                $(this).addClass("fill-blue");
+            }
+        }
+
         var io = new extIO({
             functions: {
                 js: 'absoluteSorting',
